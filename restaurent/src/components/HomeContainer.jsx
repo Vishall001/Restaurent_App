ad1b3a95483daee2300ce81e9fcd1ed8cfe45578
Vishall001 <vshmandloi@gmail.com>
2022-11-23 17:16:46 +0530

    Made the top section completely

--- restaurent/src/components/HomeContainer.jsx
+++ restaurent/src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Delivery from "../img/delivery.png";
 import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/Data";
 
 const HomeContainer = () => {
   return (
@@ -49,7 +50,31 @@ const HomeContainer = () => {
           className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
           alt="hero-bg"
         />
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((el) => (
+              <div
+                key={el.id}
+                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={el.imgSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {el.name}
+                </p>
+
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {el.desc}
+                </p>
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {el.price}
+                </p>
+              </div>
+            ))}
         </div>
       </div>
     </section>
