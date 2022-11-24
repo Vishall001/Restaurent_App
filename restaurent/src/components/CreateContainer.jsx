7d195a6c893e0f5ee19cfc7084cde142cbf7ed64
Vishall001 <vshmandloi@gmail.com>
2022-11-24 15:50:17 +0530

    Uploaded the imagesrc & all the details to firebase

--- restaurent/src/components/CreateContainer.jsx
+++ restaurent/src/components/CreateContainer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useStateValue } from "../context/StateProvider";
+
 import {
   MdFastfood,
   MdCloudUpload,
@@ -17,6 +19,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { storage } from "../firebase.config";
+import { saveItem } from "../utils/firebaseFunctions";
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
   const [calories, setCalories] = useState("");
@@ -27,7 +30,7 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  // const [{ foodItems }, dispatch] = useStateValue();
+  const [{ foodItems }, dispatch] = useStateValue();
   const uploadImage = (e) => {
     setIsLoading(true);
     const imageFile = e.target.files[0];
@@ -79,7 +82,56 @@ const CreateContainer = () => {
       }, 4000);
     });
   };
-  const saveDetails = () => {};
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if (!title || !calories || !imageAsset || !price || !category) {
+        setFields(true);
+        setMsg("Required fields can't be empty");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      } else {
+        const data = {
+          id: `${Date.now()}`,
+          title: title,
+          imageURL: imageAsset,
+          category: category,
+          calories: calories,
+          qty: 1,
+          price: price,
+        };
+        saveItem(data);
+        setIsLoading(false);
+        setFields(true);
+        setMsg("Data Uploaded successfully 😊");
+        setAlertStatus("success");
+        setTimeout(() => {
+          setFields(false);
+        }, 4000);
+        clearData();
+      }
+    } catch (err) {
+      console.log(err);
+      setFields(true);
+      setMsg("Error while uploading : Try Again 🙇");
+      setAlertStatus("danger");
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    }
+  };
+
+  const clearData = () => {
+    setTitle("");
+    setImageAsset(null);
+    setCalories("");
+    setPrice("");
+    setCategory("Select Category");
+  };
   return (
     <div className="w-full min-h-screen flex justify-center items-center ">
       <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
